59febf018328786798e339c85974e4b860c83787
rodrigojpt <rodrigojpereirat@gmail.com>
2020-11-17 10:41:10 -0500

    basic styling and test using dummy data

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 import GameCard from './Components/GameCard/GameCard';
 
 function App() {
+	const dummyGame = {
+		cover:
+			'https://i.pinimg.com/originals/1b/d4/14/1bd4141717fc3bd8d3e517cadaa231ab.png',
+		title: 'Spider-Man - Miles Morales',
+		developer: 'Insomniac Games',
+	};
 	return (
 		<div className='App center'>
-			<GameCard />
+			<GameCard game={dummyGame} />
 		</div>
 	);
 }
--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './GameCard.css';
 
-const GameCard = () => {
-	return <div>hi</div>;
+const GameCard = ({ game }) => {
+	return (
+		<div className='container game-card'>
+			<img src={game.cover} alt={game.title} />
+			<p className='game-card-title'>{game.title}</p>
+			<p className='game-card-dev'>{game.developer}</p>
+		</div>
+	);
 };
 
 export default GameCard;
