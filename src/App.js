09ae714c0b778711d2c69cd19e7cd8bfda57835d
rodrigojpt <rodrigojpereirat@gmail.com>
2020-11-17 10:53:26 -0500

    last touches and added link to component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 			'https://i.pinimg.com/originals/1b/d4/14/1bd4141717fc3bd8d3e517cadaa231ab.png',
 		title: 'Spider-Man - Miles Morales',
 		developer: 'Insomniac Games',
+		id: 12345,
 	};
 	return (
 		<div className='App center'>
--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -3,11 +3,11 @@ import './GameCard.css';
 
 const GameCard = ({ game }) => {
 	return (
-		<div className='container game-card'>
+		<a href={`games/${game.id}`} className='container game-card'>
 			<img src={game.cover} alt={game.title} />
 			<p className='game-card-title'>{game.title}</p>
 			<p className='game-card-dev'>{game.developer}</p>
-		</div>
+		</a>
 	);
 };
 
