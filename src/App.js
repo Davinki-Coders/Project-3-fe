d867954f2bf9edcbf8c8d101a05eb5f887db194c
rodrigojpt <rodrigojpereirat@gmail.com>
2020-11-17 22:31:10 -0500

    home page layout started, menu further styled, will probably swap to grid layout before MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import GameCard from './Components/GameCard/GameCard';
 import Nav from './Components/Nav/Nav';
+import Home from './Components/Home/Home';
 import { Route, Link } from 'react-router-dom';
 
 function App() {
-	const testData = require('../public/test-data.json');
 	//PLEASE DO NOTE MOVE THE HTML BELOW, IT WILL BREAK THE BURGER MENU
 	return (
 		<div className='App' id='outer-container'>
 			<Nav pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
 			<div id='page-wrap'>
-				<Link to='/' className='header-link'>
-					<h1 className='logo-header'>App Name</h1>
-				</Link>
+				<div className='logo-header'>
+					<Link to='/' className='header-link'>
+						<h1>App Name</h1>
+					</Link>
+				</div>
 				<div className='main'>
 					{/*insert youre in-progress component right above this comment if
           you'd like to test it on the home page, again please try not to move these
           components that are tied to the burger menu*/}
-					<Route path='/' />
+					<Route path='/' component={Home} />
 					<Route path='/lists' />
 					<Route path='/lists/create' />
 					<Route path='/user' />
--- src/Components/RecentLists/RecentLists.js
+++ src/Components/RecentLists/RecentLists.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ListCard from '../ListCard/ListCard';
 
-const RecentLists = () => {
-	return <div className='container'></div>;
+const RecentLists = ({ lists }) => {
+	return (
+		<div className='container'>
+			{lists.map((list, index) => (
+				<ListCard key={index} collection={list} />
+			))}
+		</div>
+	);
 };
 
 export default RecentLists;
