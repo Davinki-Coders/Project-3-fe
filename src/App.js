f6e18125fa38b1e3128c5e73186fa38064f1965e
RGordon93 <contactrashawngordon@gmail.com>
2020-11-17 19:20:16 -0500

    Completed List Card Component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ function App() {
 		image: 'https://images.theconversation.com/files/175539/original/file-20170626-315-1h7k01d.jpg?ixlib=rb-1.1.0&rect=0%2C618%2C3500%2C1697&q=45&auto=format&w=1356&h=668&fit=crop',
 		listName: 'My List',
 		author: 'Author',
+		
 	};
 	return (
 		<div className='App center'>
+			<div style={{flexDirection: 'row'}}>
 			<ListCard collection={collection} />
+			<ListCard collection={collection} />
+			<ListCard collection={collection} />
+			<ListCard collection={collection} />
+			<ListCard collection={collection} />
+			</div>
 		</div>
 	);
 }
