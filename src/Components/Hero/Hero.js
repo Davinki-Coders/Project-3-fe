b984d2bce6c6c70bd19fa618f784bc8e37a3f8ae
jake-utk <jakeryanaddis@gmail.com>
2020-11-17 19:03:00 -0600

    yet another rebase commit

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+			<div className="container center">
+				<img src='https://st.depositphotos.com/1013230/2210/i/600/depositphotos_22104925-stock-photo-game-controller-and-blue-light.jpg'></img>
+				<button>Sign Up</button>
+			</div>
+		);
+};
+
+export default Hero;
