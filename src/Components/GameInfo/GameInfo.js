042910e2857627d12dfc518b6ce528e72035c755
jake-utk <jakeryanaddis@gmail.com>
2020-11-18 15:15:36 -0600

    installed axios, useEffect

--- src/Components/GameInfo/GameInfo.js
+++ src/Components/GameInfo/GameInfo.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios'
 
 const GameInfo = () => {
+
+    useEffect(() => {
+        const id = match.params.id
+        
+
+    }, [])
+
     return (
         <div>
             Hello from GameInfo
