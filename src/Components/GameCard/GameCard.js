c7eafc2f9d974ebd9e67e822646e93a7e3697d55
jake-utk <jakeryanaddis@gmail.com>
2020-11-19 21:39:42 -0600

    Changed GameResults to CreateResults, passed formState to CreateCard

--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './GameCard.css';
+import React from "react";
+import "./GameCard.css";
 
 const GameCard = ({ game }) => {
 	return (
-		<a href={`../games/${game.id}`} className='container game-card'>
-			<img src={game.background_image} alt={game.name} />
-			<p className='game-card-title'>{game.name}</p>
-		</a>
+		<div className='card-container'>
+			<a href={`../games/${game.id}`} className='container game-card'>
+				<img src={game.background_image} alt={game.name} />
+				<p className='game-card-title'>{game.name}</p>
+			</a>
+		</div>
 	);
 };
 
