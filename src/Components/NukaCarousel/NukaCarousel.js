a16c3ce17a768a2f103c5dc9366ba539b68493d9
rodrigojpt <rodrigojpereirat@gmail.com>
2020-11-21 00:09:40 -0500

    fixed layout bug in carousel

--- src/Components/NukaCarousel/NukaCarousel.js
+++ src/Components/NukaCarousel/NukaCarousel.js
@@ -1,35 +1,40 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'nuka-carousel';
-import Axios from 'axios'
+import Axios from 'axios';
 
 const NukaCarousel = () => {
-    const [ screenshots, setScreenshots ] = useState([])
-    useEffect( () => {
-          const url =
-                'https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added?key='+ process.env.REACT_APP_RAWG_KEY;
-         Axios.get(url)
-         .then(res => {
-             return res.data.results.slice(0, 10).map(game => {
-             return game.background_image
-             })
-         })
-         .then(images => setScreenshots(images))
-         .catch(console.error)
-    },
+	const [screenshots, setScreenshots] = useState([]);
+	useEffect(() => {
+		const url =
+			'https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added?key=' +
+			process.env.REACT_APP_RAWG_KEY;
+		Axios.get(url)
+			.then((res) => {
+				return res.data.results.slice(0, 10).map((game) => {
+					return game.background_image;
+				});
+			})
+			.then((images) => setScreenshots(images))
+			.catch(console.error);
+	}, []);
 
-    []
-    )
-    
-        return (
-					<Carousel
-						withoutControls={true}
-						autoplay={true}
-						autoplayInterval={5000}>
-						<img src='https://media.rawg.io/media/resize/1920/-/screenshots/092/092c8c633cff8e366b7b7e456337303e_mOIc8Dp.jpg' />
-                        {screenshots.map( screenshot => <img src={screenshot}/>)}
-					</Carousel>
-				);
-    
-}
+	return (
+		<Carousel withoutControls={true} autoplay={true} autoplayInterval={5000}>
+			<img
+				className='hero-image'
+				src='https://media.rawg.io/media/resize/1920/-/screenshots/092/092c8c633cff8e366b7b7e456337303e_mOIc8Dp.jpg'
+				alt='Demon Souls'
+			/>
+			{screenshots.map((screenshot, index) => (
+				<img
+					src={screenshot}
+					className='hero-image'
+					key={index}
+					alt={'App Carousel'}
+				/>
+			))}
+		</Carousel>
+	);
+};
 
-export default NukaCarousel
+export default NukaCarousel;
