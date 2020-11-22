bff9515d8c34ee3c376512f54fe822b70e665912
jake-utk <jakeryanaddis@gmail.com>
2020-11-22 16:28:34 -0600

    Fixed useEffect in Home.js to display 3 most recently added lists from database

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,31 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import RecentLists from '../RecentLists/RecentLists';
-import { Link } from 'react-router-dom';
-import Hero from '../Hero/Hero';
-import './Home.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import RecentLists from "../RecentLists/RecentLists";
+import { Link } from "react-router-dom";
+import Hero from "../Hero/Hero";
+import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
 	const [recentLists, setRecentLists] = useState([]);
 
-
-	// THIS NEEDS TESTING ONCE WE HAVE MORE LISTS IN DB
 	useEffect(() => {
-		axios.get('https://davinkibackend.herokuapp.com/api/lists').then((res) => {
-			const list = [];
-			console.log('res:', res);
-			//update this to only show like 3-5 lists
-			console.log('length of response array:', res.data.length);
-
-			for (let i = 0; i < 3; i++) {
-				const index = Math.floor(Math.random(0, res.data.length + 1) * res.data.length)
-				console.log('index:', index);
-				list.push(res.data[index]);
-			}
-			console.log('list:', list);
-			setRecentLists(list);
+		axios.get("https://davinkibackend.herokuapp.com/api/lists").then((res) => {
+			setRecentLists(res.data.slice(Math.max(res.data.length - 3, 0)));
 		});
 	}, []);
+
 	return (
 		<div className='home'>
 			<Hero />
