f4a21e1d111a6dd5339784467d5b4324351b00ec
rodrigojpt <rodrigojpereirat@gmail.com>
2020-11-22 19:30:04 -0500

    finished coniditional link for list editing

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import GameCard from '../GameCard/GameCard';
 import axios from 'axios';
 import './List.css';
+import { AppContext } from '../../AppContext';
+import { Link } from 'react-router-dom';
 
 const List = ({ id }) => {
 	const [list, setList] = useState({});
-
+	const { userInfo } = useContext(AppContext);
 	useEffect(() => {
 		axios
 			.get('https://davinkibackend.herokuapp.com/api/lists/' + id)
@@ -23,6 +25,9 @@ const List = ({ id }) => {
 			<h2>{list.title}</h2>
 			<h3>by {list.author}</h3>
 			<p>{list.description}</p>
+			{userInfo._id === list.owner ? (
+				<Link to={'/lists/edit/' + id}>Edit List</Link>
+			) : null}
 			<div className='user-list-container'>
 				{list.games.map((game, index) => (
 					<GameCard game={game} key={index} />
